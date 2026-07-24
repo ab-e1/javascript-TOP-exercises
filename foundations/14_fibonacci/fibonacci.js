5b42871fca54f7ffe7c724c9a98c63834150f37b
ab-e1 <abelghy17@gmail.com>
2026-07-24 19:00:12 +0300

    donw with fibonacci used loops and if logic

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,23 @@
-const fibonacci = function() {
+const fibonacci = function (n) {
+  let i = 0;
+  let febArr = [];
+  let c = 0;
 
+  while (i <= 10) {
+    if (i <= 1) {
+      febArr.push(1);
+    } else {
+      let a = i - 2;
+      let b = i - 1;
+      let d = Number(febArr[a]);
+      let e = Number(febArr[b]);
+      c = d + e;
+      febArr.push(c);
+    }
+    i++;
+  }
+  let N = n - 1;
+  return febArr[N];
 };
 
 // Do not edit below this line
