c39c7c90a9678e3de1eb855c7f8fc00fe6814b5a
ab-e1 <abelghy17@gmail.com>
2026-07-24 20:56:13 +0300

    solved, with using a while loop and using push , to push to the new array

--- foundations/15_getTheTitles/getTheTitles.js
+++ foundations/15_getTheTitles/getTheTitles.js
@@ -1,5 +1,12 @@
-const getTheTitles = function() {
-
+const getTheTitles = function (array) {
+  let i = 0;
+  let length = Number(array.length);
+  let newArray = [];
+  while (i < length) {
+    newArray.push(array[i].title);
+    i++;
+  }
+  return newArray;
 };
 
 // Do not edit below this line
