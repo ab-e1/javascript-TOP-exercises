c18f971a4b8b0d75ec57ce352f498663e602b688
ab-e1 <abelghy17@gmail.com>
2026-07-24 18:00:16 +0300

    loops and if logic , done with palindrom

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,31 @@
-const palindromes = function () {
+const palindromes = function (str) {
+  let length = Number(str.length);
+  let noSpaceStr = "";
+  let j = 0;
+  while (j < length) {
+    if (str[j] == " ") {
+      noSpaceStr = noSpaceStr;
+    } else {
+      noSpaceStr = noSpaceStr + str[j];
+    }
+    j++;
+  }
 
+  let i = length - 1;
+  let revStr = "";
+  while (i >= 0) {
+    if (str[i] == " ") {
+      revStr = revStr;
+    } else {
+      revStr = revStr + str[i];
+    }
+    i--;
+  }
+  if (noSpaceStr == revStr) {
+    return true;
+  } else {
+    return false;
+  }
 };
 
 // Do not edit below this line
