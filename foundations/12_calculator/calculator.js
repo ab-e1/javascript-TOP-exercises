e4111b587288ca910bd449d09de6609a95f2a6e2
ab-e1 <abelghy17@gmail.com>
2026-07-24 17:29:58 +0300

    calculator done, used a combination of while loop and if logic for teh factorial

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,25 +1,51 @@
-const add = function() {
-	
+const add = function (a, b) {
+  let sum = a + b;
+  return sum;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+  let difference = a - b;
+  return difference;
 };
 
-const sum = function() {
-	
+const sum = function (array) {
+  let i = 0;
+  let length = Number(array.length);
+  let arrSum = 0;
+  while (i < length) {
+    arrSum = arrSum + array[i];
+    i++;
+  }
+  return arrSum;
 };
 
-const multiply = function() {
-
+const multiply = function (array) {
+  let i = 0;
+  let length = Number(array.length);
+  let arrSum = 1;
+  while (i < length) {
+    arrSum = arrSum * array[i];
+    i++;
+  }
+  return arrSum;
 };
 
-const power = function() {
-	
+const power = function (a, b) {
+  let pow = a ** b;
+  return pow;
 };
 
-const factorial = function() {
-	
+const factorial = function (n) {
+  let i = n;
+  let result = n;
+  while (i > 0) {
+    let son = i - 1;
+    if (son > 0) {
+      result = result * son;
+    }
+    i--;
+  }
+  return result;
 };
 
 // Do not edit below this line
@@ -29,5 +55,13 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
+/* 
+console.log(factorial(5));
+console.log(multiply([1, 2, 3, 4, 5]));
+console.log(sum([1, 2, 3, 4, 5]));
+console.log(power(2, 3));
+console.log(subtract(5, 3));
+console.log(add(3,5));
+ */
