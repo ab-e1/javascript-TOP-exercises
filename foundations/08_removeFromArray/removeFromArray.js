6ac37920849f94ab9dc7997ae9ece872c1226e2d
ab-e1 <abelghy17@gmail.com>
2026-07-24 16:13:53 +0300

    implemented a function to remove a value from an array , with out using builtin methods

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function (array, num) {
+  let arrLength = Number(array.length);
+  i = 0;
+  let newArray = [];
+  while (i < arrLength) {
+    if (array[i] != num) {
+      newArray.push(array[i]);
+    }
+    i++;
+  }
+  return newArray;
 };
 
 // Do not edit below this line
